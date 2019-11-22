1b6864f905cbea056f71afbb3c2062864af6068c
Eva Hsu <eva@cybavo.com>
2019-11-22 11:29:53 +0800

    revert mis-merged code

--- WalletSDKDemo/PIN/PinInputViewController.swift
+++ WalletSDKDemo/PIN/PinInputViewController.swift
@@ -16,50 +16,64 @@ class PinInputViewController: UIViewController{
     @IBOutlet weak var forgotButton: UIButton!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var stackView: UIStackView!
+    var pinInputIndecatorView: PinInputIndicatorView?
     //MARK: Property
-    var pinInputView: PinInputView!
+    var pinInputView: NumericPinCodeInputView?
     let kPasswordDigit = 6
     
     override func viewDidLoad() {
         super.viewDidLoad()
         titleLabel.text = titleText
         forgotButton.isHidden = hideForgot
-        pinInputView = PinInputView.create(in: stackView, digit: kPasswordDigit)
-        pinInputView.setOnPinInputListener(delegate: self)
+        pinInputIndecatorView = PinInputIndicatorView(frame: CGRect(x: titleLabel.bounds.minX, y: titleLabel.bounds.minY + titleLabel.bounds.height + 10, width: 250, height: 35))
+        
+        
+        pinInputView = NumericPinCodeInputView(frame: CGRect(x: pinInputIndecatorView!.bounds.minX, y: pinInputIndecatorView!.bounds.minY + pinInputIndecatorView!.bounds.height + 10, width: 250, height: 400))
+        pinInputView?.setOnPinInputListener(delegate: self)
         let lightGray = UIColor(red: 227.0/255.0, green: 227.0/255.0, blue: 227.0/255.0, alpha: 1.0)
         let darkGray = UIColor(red: 59.0/255.0, green: 59.0/255.0, blue: 58.0/255.0, alpha: 1.0)
-        
-        pinInputView.styleAttr = StyleAttr(
+  
+        pinInputView?.styleAttr = CYBAVOWallet.StyleAttr(
             fixedOrder: false,
+            disabled: false,
             buttonWidth: 70,
             buttonHeight: 70,
+            horizontalSpacing: 5,
+            verticalSpacing: 7,
             buttonTextColor: darkGray,
             buttonTextColorPressed: lightGray,
+            buttonTextColorDisabled: UIColor.red,
             buttonBackgroundColor: lightGray,
             buttonBackgroundColorPressed: darkGray,
-            buttonBorderRadius: 72,
+            buttonBackgroundColorDisabled: UIColor.yellow,
+            buttonBorderRadius: 33,
             buttonBorderWidth: 0,
-            buttonBorderColor: UIColor.red,
+            buttonBorderColor: UIColor.green,
             buttonBorderColorPressed: UIColor.blue,
+            buttonBorderColorDisabled: UIColor.orange,
             backspaceButtonWidth: 70,
             backspaceButtonHeight: 70,
-            backspaceButtonTextFont: nil,
             backspaceButtonTextColor: darkGray,
             backspaceButtonTextColorPressed: lightGray,
+            backspaceButtonTextColorDisabled: UIColor.yellow,
             backspaceButtonBackgroundColor: lightGray,
             backspaceButtonBackgroundColorPressed: darkGray,
-            backspaceButtonBorderRadius: 72,
+            backspaceButtonBackgroundColorDisabled: UIColor.red,
+            backspaceButtonBorderRadius: 33,
             backspaceButtonBorderWidth: 0,
             backspaceButtonBorderColor: UIColor.blue,
             backspaceButtonBorderColorPressed: UIColor.red,
-            indicatorStrokeColor: darkGray,
-            indicatorFillColor: lightGray,
-            indicatorHeight: 17,
-            UnfilledPlaceholder: "-",
-            filledPlaceholder: "*",
-            backspaceButtonText: "⌫")
-//        pinInputView.deleteButtonLocalizedTitle = "⌫"
+            backspaceButtonBorderColorDisabled: UIColor.black)
+       
+        pinInputIndecatorView!.strokeColor = darkGray
+        pinInputIndecatorView!.fillColor = lightGray
+        pinInputIndecatorView!.drawText = true
+        pinInputIndecatorView!.filledPlaceholder = "*"
+        pinInputIndecatorView!.unfilledPlaceholder = "-"
         
+        stackView.addArrangedSubview(pinInputIndecatorView!)
+        stackView.addArrangedSubview(pinInputView!)
+    
     }
     @IBAction func onCancle(_ sender: Any) {
         dismiss(animated: true, completion: nil)
@@ -74,10 +88,11 @@ class PinInputViewController: UIViewController{
     }
 }
 
-extension PinInputViewController: OnPinInputListener {
+extension PinInputViewController: CYBAVOWallet.OnPinInputListener {
     func onChanged(length: Int) {
-        if(length == pinInputView.getMaxLength()){
-            var pinSecret = pinInputView.submit()
+        pinInputIndecatorView!.inputDotCount = length
+        if(length == pinInputView!.getMaxLength()){
+            var pinSecret = pinInputView!.submit()
             callback?(pinSecret)
             dismiss(animated: true, completion: nil)
         }
